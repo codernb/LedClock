b4c3915f39aa28fc0bf29165ccd65247bc277574
codernb <cyril.halmo@students.fhnw.ch>
2016-02-19 07:19:16 +0100

    tinkering

--- .ino.cpp
+++ .ino.cpp
@@ -1,12 +1,13 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-02-19 05:34:44
+//This file has been generated on 2016-02-19 06:56:23
 
 #include "Arduino.h"
 #include <Arduino.h>
 void setup() ;
 void loop() ;
+void time() ;
 void setMinLed(byte pin) ;
 void setHourLed(byte pin) ;
 void lightLevel() ;
--- LedClock.ino
+++ LedClock.ino
@@ -2,30 +2,49 @@
 
 #define PHOTO_RESISTOR_PIN 0
 
-byte ledMap[72][2] = { { 7, 8 }, { 8, 7 }, { 7, 9 }, { 9, 7 } };
+byte ledMap[72][2] = { { 1, 2 }, { 2, 1 }, { 3, 2 }, { 2, 3 }, { 3, 4 },
+		{ 4, 3 }, { 5, 4 }, { 4, 5 }, { 5, 6 }, { 6, 5 }, { 7, 6 }, { 6, 7 }, {
+				7, 8 }, { 8, 7 }, { 9, 8 }, { 8, 9 }, { 9, 7 }, { 7, 9 },
+		{ 5, 7 }, { 7, 5 }, { 5, 3 }, { 3, 5 }, { 1, 3 }, { 3, 1 }, { 1, 4 }, {
+				4, 1 }, { 7, 4 }, { 4, 7 }, { 7, 2 }, { 2, 7 }, { 4, 2 },
+		{ 2, 4 }, { 4, 6 }, { 6, 4 }, { 8, 6 }, { 6, 8 }, { 8, 5 }, { 5, 8 }, {
+				2, 5 }, { 5, 2 }, { 2, 9 }, { 9, 2 }, { 6, 9 }, { 9, 6 },
+		{ 6, 3 }, { 3, 6 }, { 9, 3 }, { 3, 9 }, { 9, 5 }, { 5, 9 }, { 1, 5 }, {
+				5, 1 }, { 1, 6 }, { 6, 1 }, { 2, 6 }, { 6, 2 }, { 2, 8 },
+		{ 8, 2 }, { 4, 8 }, { 8, 4 }, { 7, 1 }, { 1, 7 }, { 7, 3 }, { 3, 7 }, {
+				8, 3 }, { 3, 8 }, { 8, 1 }, { 1, 8 }, { 9, 1 }, { 1, 9 },
+		{ 9, 4 }, { 4, 9 } };
 
-byte hourLed;
-byte minLed;
+byte minLed = 255;
+byte hourLed = 255;
 
 bool alternate;
 byte ledDelay;
 
 void setup() {
-	;
+	Serial.begin(9600);
 }
 
 void loop() {
+	time();
 	lightLevel();
 	light();
+}
 
-	if (millis() / 2000 % 2 == 0) {
-		setMinLed(2);
-		setHourLed(1);
-	} else {
-		setMinLed(0);
-		setHourLed(3);
+void time() {
+	unsigned long int ms = millis();
+	byte min = ms / 60000 % 60;
+	byte hour = ms / 3600000 % 12;
+	if (min != minLed) {
+		Serial.print("min: ");
+		Serial.println(min);
+		setMinLed(min);
 	}
-
+	if (hour + 60 != hourLed) {
+		Serial.print("h: ");
+		Serial.println(hour + 60);
+		setHourLed(hour + 60);
+	}
 }
 
 void setMinLed(byte pin) {
