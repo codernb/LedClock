249ebbd4d76db2cfc06041d08e9e9ecc68f9f99d
codernb <cyril.halmo@students.fhnw.ch>
2016-02-20 00:43:55 +0100

    almost done

--- .ino.cpp
+++ .ino.cpp
@@ -1,15 +1,18 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-02-19 06:56:23
+//This file has been generated on 2016-02-19 23:45:06
 
 #include "Arduino.h"
 #include <Arduino.h>
 void setup() ;
 void loop() ;
 void time() ;
-void setMinLed(byte pin) ;
+void setMinuteLed(byte pin) ;
 void setHourLed(byte pin) ;
+void buttons() ;
+void addMinOffset() ;
+void addHourOffset() ;
 void lightLevel() ;
 void light() ;
 void setActive(byte led) ;
--- LedClock.ino
+++ LedClock.ino
@@ -1,89 +1,109 @@
 #include <Arduino.h>
 
 #define PHOTO_RESISTOR_PIN 0
+#define MAX_DELAY 13
+#define MIN_DELAY 0
+#define DOWN_BARRIER (downBarrierValue - ledDelay) * 50
+#define UP_BARRIER (upBarrierValue - ledDelay) * 50
+#define MINUTE_BUTTON_PIN 12
+#define HOUR_BUTTON_PIN 13
 
-byte ledMap[72][2] = { { 1, 2 }, { 2, 1 }, { 3, 2 }, { 2, 3 }, { 3, 4 },
-		{ 4, 3 }, { 5, 4 }, { 4, 5 }, { 5, 6 }, { 6, 5 }, { 7, 6 }, { 6, 7 }, {
-				7, 8 }, { 8, 7 }, { 9, 8 }, { 8, 9 }, { 9, 7 }, { 7, 9 },
-		{ 5, 7 }, { 7, 5 }, { 5, 3 }, { 3, 5 }, { 1, 3 }, { 3, 1 }, { 1, 4 }, {
-				4, 1 }, { 7, 4 }, { 4, 7 }, { 7, 2 }, { 2, 7 }, { 4, 2 },
-		{ 2, 4 }, { 4, 6 }, { 6, 4 }, { 8, 6 }, { 6, 8 }, { 8, 5 }, { 5, 8 }, {
-				2, 5 }, { 5, 2 }, { 2, 9 }, { 9, 2 }, { 6, 9 }, { 9, 6 },
-		{ 6, 3 }, { 3, 6 }, { 9, 3 }, { 3, 9 }, { 9, 5 }, { 5, 9 }, { 1, 5 }, {
-				5, 1 }, { 1, 6 }, { 6, 1 }, { 2, 6 }, { 6, 2 }, { 2, 8 },
-		{ 8, 2 }, { 4, 8 }, { 8, 4 }, { 7, 1 }, { 1, 7 }, { 7, 3 }, { 3, 7 }, {
-				8, 3 }, { 3, 8 }, { 8, 1 }, { 1, 8 }, { 9, 1 }, { 1, 9 },
-		{ 9, 4 }, { 4, 9 } };
+const byte ledMap[72][2] = { { 1, 2 }, { 2, 1 }, { 3, 2 }, { 2, 3 }, { 3, 4 }, {
+		4, 3 }, { 5, 4 }, { 4, 5 }, { 5, 6 }, { 6, 5 }, { 7, 6 }, { 6, 7 }, { 7,
+		8 }, { 8, 7 }, { 9, 8 }, { 8, 9 }, { 9, 7 }, { 7, 9 }, { 5, 7 },
+		{ 7, 5 }, { 5, 3 }, { 3, 5 }, { 1, 3 }, { 3, 1 }, { 1, 4 }, { 4, 1 }, {
+				7, 4 }, { 4, 7 }, { 7, 2 }, { 2, 7 }, { 4, 2 }, { 2, 4 },
+		{ 4, 6 }, { 6, 4 }, { 8, 6 }, { 6, 8 }, { 8, 5 }, { 5, 8 }, { 2, 5 }, {
+				5, 2 }, { 2, 9 }, { 9, 2 }, { 6, 9 }, { 9, 6 }, { 6, 3 },
+		{ 3, 6 }, { 9, 3 }, { 3, 9 }, { 9, 5 }, { 5, 9 }, { 1, 5 }, { 5, 1 }, {
+				1, 6 }, { 6, 1 }, { 2, 6 }, { 6, 2 }, { 2, 8 }, { 8, 2 },
+		{ 4, 8 }, { 8, 4 }, { 7, 1 }, { 1, 7 }, { 7, 3 }, { 3, 7 }, { 8, 3 }, {
+				3, 8 }, { 8, 1 }, { 1, 8 }, { 9, 1 }, { 1, 9 }, { 9, 4 },
+		{ 4, 9 } };
 
-byte minLed = 255;
-byte hourLed = 255;
+const byte downBarrierValue = MAX_DELAY + 2;
+const byte upBarrierValue = MAX_DELAY + 3;
+
+byte minuteLed;
+byte hourLed;
+byte minuteOffset;
+byte hourOffset;
 
 bool alternate;
-byte ledDelay;
+byte ledDelay = 15;
+
+bool minuteButtonPressed;
+bool hourButtonPressed;
 
 void setup() {
-	Serial.begin(9600);
+	pinMode(MINUTE_BUTTON_PIN, INPUT);
 }
 
 void loop() {
 	time();
-	lightLevel();
+//	lightLevel();
 	light();
+//	buttons();
 }
 
 void time() {
 	unsigned long int ms = millis();
-	byte min = ms / 60000 % 60;
-	byte hour = ms / 3600000 % 12;
-	if (min != minLed) {
-		Serial.print("min: ");
-		Serial.println(min);
-		setMinLed(min);
-	}
-	if (hour + 60 != hourLed) {
-		Serial.print("h: ");
-		Serial.println(hour + 60);
-		setHourLed(hour + 60);
-	}
+//	byte min = (ms / 60000 + minuteOffset) % 60;
+	byte min = (ms / 1000 + minuteOffset) % 60;
+//	byte hour = (ms / 3600000 + hourOffset) % 12;
+	byte hour = (ms / 3600 + hourOffset) % 12;
+	setMinuteLed(min);
+	setHourLed(hour);
 }
 
-void setMinLed(byte pin) {
-	setToInput(minLed);
-	minLed = pin;
+void setMinuteLed(byte pin) {
+	if (pin == minuteLed)
+		return;
+	setToInput(minuteLed);
+	minuteLed = pin;
 }
 
 void setHourLed(byte pin) {
-	setToInput(hourLed);
+	pin += 60;
+	if (pin == hourLed)
+		return setToInput(hourLed);
 	hourLed = pin;
+}
+
+void buttons() {
+	byte minuteButtonState = digitalRead(MINUTE_BUTTON_PIN);
+	byte houreButtonState = digitalRead(HOUR_BUTTON_PIN);
+	if (!minuteButtonPressed && minuteButtonState == HIGH) {
+		addMinOffset();
+		minuteButtonPressed = true;
+	} else if (minuteButtonState == LOW) {
+		minuteButtonPressed = false;
+	}
+}
+
+void addMinOffset() {
+	minuteOffset = (minuteOffset + 1) % 60;
+}
+
+void addHourOffset() {
+	hourOffset = (hourOffset + 1) % 12;
 }
 
 void lightLevel() {
 	int photoResistorValue = analogRead(PHOTO_RESISTOR_PIN);
-	switch (ledDelay) {
-	case (0):
-		if (photoResistorValue < 200)
-			ledDelay = 1;
-		break;
-	case (1):
-		if (photoResistorValue > 300)
-			ledDelay = 0;
-		else if (photoResistorValue < 100)
-			ledDelay = 2;
-		break;
-	case (2):
-		if (photoResistorValue > 200)
-			ledDelay = 1;
-		break;
-	}
+	if (ledDelay < MAX_DELAY && photoResistorValue < DOWN_BARRIER)
+		ledDelay++;
+	else if (ledDelay > MIN_DELAY && photoResistorValue > UP_BARRIER)
+		ledDelay--;
 }
 
 void light() {
 	if (alternate) {
 		setToInput(hourLed);
 		delay(ledDelay);
-		setActive(minLed);
+		setActive(minuteLed);
 	} else {
-		setToInput(minLed);
+		setToInput(minuteLed);
 		delay(ledDelay);
 		setActive(hourLed);
 	}
