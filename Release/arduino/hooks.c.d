f6409345b2a44d15955e88a1a5eebd99214dabf9
codernb <cyril.halmo@students.fhnw.ch>
2016-03-02 20:41:20 +0100

    removes

--- Release/arduino/hooks.c.d
+++
@@ -1,2 +0,0 @@
-arduino/hooks.c.o: \
- /home/cyril/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/hooks.c
--- spec.d
+++
@@ -1,2 +0,0 @@
-spec.o: \
- /home/cyril/EclipseProjects/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
