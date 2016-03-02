281353474d9b762268eacd6c964822b6167a15c9
codernb <cyril.halmo@students.fhnw.ch>
2016-03-02 21:08:48 +0100

    refactoring

--- Release/arduino/WString.cpp.d
+++
@@ -1,5 +0,0 @@
-arduino/WString.cpp.o: \
- /home/cyril/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/WString.cpp \
- /home/cyril/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/WString.h
-
-/home/cyril/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/WString.h:
